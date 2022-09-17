7ef18c417c6c6dee15e90b5a7fce3083533bd906
etsh <mohamed.hesham2105@gmail.com>
2022-09-17 23:00:53 +0200

    final

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3897cbf345fc4ac6bc6635d982f4a19-125892616.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
